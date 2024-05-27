702c65d8367baf09af12cba70b8546a8beb60a88
diogosantos <dpsantos005@gmail.com>
2024-05-27 14:55:17 +0100

    display de scores com codigo do tgp

--- software_lic/src/main/kotlin/SpaceInvadersApp.kt
+++ software_lic/src/main/kotlin/SpaceInvadersApp.kt
@@ -1,17 +1,52 @@
 import isel.leic.utils.Time
+import java.io.File
 
 object SpaceInvadersApp {
+    const val NAME_FILE = "statistics.txt"
     private var list0: String = "" // Lista para armazenar números aleatórios na linha 0
     private var list1: String = "" // Lista para armazenar números aleatórios na linha 1
     private var score: Int = 0
+    private var coin: Int = 0
     private var randomNumber = (0..9).random().toString() // Gera um número aleatório entre 0 e 9
     private var randomLine = (0..1).random() // Gera uma linha aleatória (0 ou 1)
     private var line = 0
-    private val row = 1
+    private const val row = 1
     private var shootingKey = ' ' // Inicializa a tecla de tiro
     private var hit = false
-    val interval = 1000L // Intervalo de tempo para gerar novos números aleatórios (1000 ms)
-    var lastUpdateTime = System.currentTimeMillis() // Marca o tempo da última atualização (Começo do jogo)
+    private const val interval = 1000L // Intervalo de tempo para gerar novos números aleatórios (1000 ms)
+    private var lastUpdateTime = System.currentTimeMillis() // Marca o tempo da última atualização (Começo do jogo)
+    var COIN_MASK = 0x40
+    val text = File(SpaceInvadersApp.NAME_FILE).readLines()
+
+    fun isCoin(){
+        if (HAL.isBit(COIN_MASK)){
+            println(coin + 1)
+            HAL.clrBits(COIN_MASK)
+        }
+    }
+
+    fun displayStatistics() {
+        // Criar uma lista de pares (nome, pontuação)
+        val scores = text.map {
+            val (name, score) = it.split(";")
+            name to score.toInt()
+        }.sortedByDescending { it.second } // Ordenar pela pontuação em ordem decrescente
+
+        var position = 1
+
+        for ((name, score) in scores) {
+            isCoin()
+            if (KBD.getKey() == '*') {
+                LCD.clear()
+                playing()
+                return
+            }
+            LCD.cursor(1, 0)
+            LCD.write(" $position $name $score           ")
+            position++
+            Time.sleep(1000)
+        }
+    }
 
     // Função para inicializar os componentes
     fun init() {
@@ -20,6 +55,7 @@ object SpaceInvadersApp {
         LCD.write(" Space Invaders ") // Escreve a mensagem inicial na primeira linha do LCD
         LCD.cursor(1, 0)
         LCD.write(" Game X  X X  0$ ") // Escreve uma segunda mensagem na segunda linha do LCD
+        Time.sleep(2000)
     }
 
     fun addScore(score: Int): Int {
@@ -28,7 +64,7 @@ object SpaceInvadersApp {
         return score + 1
     }
 
-     fun addInvaders(randomLine: Int, randomNumber: String) {
+    fun addInvaders(randomLine: Int, randomNumber: String) {
         if (randomLine == 0) {
             list0 += randomNumber
         } else {
@@ -37,7 +73,7 @@ object SpaceInvadersApp {
         TUI.displayWrite(list0, list1, randomLine, 1, hit)
     }
 
-     fun updateTime() {
+    fun updateTime() {
         val currentTime = System.currentTimeMillis()
         if (currentTime - lastUpdateTime >= interval) {
             randomLine = (0..1).random()
@@ -47,7 +83,7 @@ object SpaceInvadersApp {
         }
     }
 
-     fun changeLine() {
+    fun changeLine() {
         LCD.cursor(line, row)
         LCD.write(" ")
         line = if (line == 0) 1 else 0
@@ -55,7 +91,7 @@ object SpaceInvadersApp {
         LCD.cursor(line, row)
     }
 
-     fun checkHit() {
+    fun checkHit() {
         if (line == 0 && list0.isNotEmpty() && shootingKey == list0[0]) {
             score = addScore(score)
             list0 = list0.substring(1)
@@ -67,14 +103,13 @@ object SpaceInvadersApp {
         }
     }
 
-     fun displayKey(key: Char) {
+    fun displayKey(key: Char) {
         if (key != KBD.NONE) {
             LCD.cursor(line, 0)
             LCD.write(key)
             shootingKey = key
         }
     }
-
 
     fun handleKeyPress(key: Char) {
         when (key) {
@@ -102,9 +137,11 @@ object SpaceInvadersApp {
 fun main() {
     SpaceInvadersApp.init()
     while (true) {
-        if (KBD.getKey() == '*') { // Espera pela tecla '*' para iniciar o jogo
+        SpaceInvadersApp.displayStatistics()
+        SpaceInvadersApp.isCoin()
+        if (KBD.getKey() == '*') {
             LCD.clear()
-            SpaceInvadersApp.playing() // Inicia o jogo
+            SpaceInvadersApp.playing()
             break
         }
     }
